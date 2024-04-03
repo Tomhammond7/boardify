49117e77c0a4aaf8ef2a8ff95c42a5526f82fb12
Thomas Hammond <Thomas.Hammond@gl-education.com>
2024-04-03 16:51:18 +0100

    Fixed close div error and added additional functionality.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,163 +1,3 @@
-// import { Component, ChangeDetectorRef, OnInit, ElementRef, ViewChild} from '@angular/core';
-// import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
-// import { MatDialog } from '@angular/material/dialog';
-// import { ItemDialogComponent } from '../item-dialog/item-dialog.component';
-
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.less']
-// })
-// export class HomeComponent implements OnInit {
-//   todoItems: { value: string, editing: boolean }[] = [
-//     { value: 'Item 1', editing: false },
-//     { value: 'Item 2', editing: false },
-//     { value: 'Item 3', editing: false }
-//   ];
-//   inProgressItems: { value: string; editing: boolean; }[] = [];
-//   doneItems: { value: string; editing: boolean; }[] = [];
-//   newTodoItem: string = '';
-//   newInProgressItem: string = '';
-//   newDoneItem: string = '';
-//   todoAdding: boolean = false;
-//   inProgressAdding: boolean = false;
-//   doneAdding: boolean = false;
-//   @ViewChild('todoBtn') todoBtn!: ElementRef;
-//   @ViewChild('inProgressBtn') inProgressBtn!: ElementRef;
-//   @ViewChild('doneBtn') doneBtn!: ElementRef;
-
-//   constructor(private cdr: ChangeDetectorRef, 
-//     private dialog: MatDialog, 
-//     private elementRef: ElementRef) {}
-
-//   ngOnInit(): void {
-//     this.loadDataFromLocalStorage();
-//   }
-
-//   drop(event: CdkDragDrop<{ value: string; editing: boolean; }[]>) {
-//     if (event.container.data) {
-//       if (event.previousContainer === event.container) {
-//         moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-//       } else {
-//         transferArrayItem(
-//           event.previousContainer.data,
-//           event.container.data,
-//           event.previousIndex,
-//           event.currentIndex
-//         );
-
-//         this.saveChangesToLocalStorage();
-//       }
-//     }
-//   }
-
-//   openTodoDialog(): void {
-//     this.openDialog(this.todoBtn.nativeElement as HTMLElement);
-//   }
-
-//   openInProgressDialog(): void {
-//     this.openDialog(this.inProgressBtn.nativeElement as HTMLElement);
-//   }
-
-//   openDoneDialog(): void {
-//     this.openDialog(this.doneBtn.nativeElement as HTMLElement);
-//   }
-
-//   private openDialog(buttonRef: HTMLElement): void {
-//     const rect = buttonRef.getBoundingClientRect();
-
-//     const dialogPosition = {
-//       top: `${rect.bottom}px`,
-//       left: `${rect.left}px`
-//     };
-
-//     const dialogRef = this.dialog.open(ItemDialogComponent, {
-//       width: '300px',
-//       height: '500px',
-//       position: dialogPosition,
-//       panelClass: 'item-dialog'
-//     });
-
-//     dialogRef.afterClosed().subscribe(result => {
-//       console.log('Dialog closed with result:', result);
-//     });
-//   }
-
-//   addNewItem(list: any[], newItemValue: string, type: string) {
-//     if (newItemValue.trim() !== '') {
-//       list.push({ value: newItemValue, editing: false });
-
-//       if (type === 'todo') {
-//         this.newTodoItem = '';
-//       } else if (type === 'inProgress') {
-//         this.newInProgressItem = '';
-//       } else if (type === 'done') {
-//         this.newDoneItem = '';
-//       }
-//       this.saveChangesToLocalStorage();
-//     }
-//     this.closeNewItemDiv(type);
-//   }
-
-//   updateItem(list: any[], index: number) {
-//     list[index].editing = false;
-//     this.saveChangesToLocalStorage();
-//   }
-
-//   startEditing(item: { value: string, editing: boolean }) {
-//     item.editing = true;
-//   }
-
-//   startAdding(type: string) {
-//     if (type === 'todo') {
-//       this.todoAdding = true;
-//     } else if (type === 'inProgress') {
-//       this.inProgressAdding = true;
-//     } else if (type === 'done') {
-//       this.doneAdding = true;
-//     }
-//   }
-
-//   closeNewItemDiv(type: string) {
-//     if (type === 'todo') {
-//       this.todoAdding = !this.todoAdding;
-//     } else if (type === 'inProgress') {
-//       this.inProgressAdding = !this.inProgressAdding;
-//     } else if (type === 'done') {
-//       this.doneAdding = !this.doneAdding;
-//     }
-//   }
-
-//   saveChanges(index: number, list: any[]) {
-//     list[index].editing = false;
-//     this.saveChangesToLocalStorage();
-//   }
-
-//   saveChangesToLocalStorage() {
-//     localStorage.setItem('todoItems', JSON.stringify(this.todoItems));
-//     localStorage.setItem('inProgressItems', JSON.stringify(this.inProgressItems));
-//     localStorage.setItem('doneItems', JSON.stringify(this.doneItems));
-//   }
-
-//   loadDataFromLocalStorage() {
-//     const todoItems = localStorage.getItem('todoItems');
-//     const inProgressItems = localStorage.getItem('inProgressItems');
-//     const doneItems = localStorage.getItem('doneItems');
-
-//     if (todoItems) {
-//       this.todoItems = JSON.parse(todoItems);
-//     }
-
-//     if (inProgressItems) {
-//       this.inProgressItems = JSON.parse(inProgressItems);
-//     }
-
-//     if (doneItems) {
-//       this.doneItems = JSON.parse(doneItems);
-//     }
-//   }
-// }
-
 import { Component, ChangeDetectorRef, OnInit, ElementRef, ViewChild} from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { MatDialog } from '@angular/material/dialog';
@@ -170,11 +10,11 @@ import { ItemDialogComponent } from '../item-dialog/item-dialog.component';
 })
 export class HomeComponent implements OnInit {
   columns: { name: string, items: { value: string, editing: boolean }[], adding: boolean }[] = [];
-
   newColumnTitle: string = '';
   newItemValue: string = '';
   newItemValues: { [key: string]: string } = {};
   addNewColumn: boolean = false;
+  selectedColumnIndex: number | null = null;
 
   @ViewChild('addColumnBtn') addColumnBtn!: ElementRef;
 
@@ -188,13 +28,17 @@ export class HomeComponent implements OnInit {
   }
 
   initializeColumns() {
-    if (!localStorage.getItem('columns')) {
+    const columnsFromLocalStorage = localStorage.getItem('columns');
+    
+    if (!columnsFromLocalStorage) {
       this.columns = [
         { name: 'To do', items: [], adding: false },
         { name: 'In Progress', items: [], adding: false },
         { name: 'Done', items: [], adding: false }
       ];
       this.saveChangesToLocalStorage();
+    } else {
+      this.columns = JSON.parse(columnsFromLocalStorage);
     }
   }
 
@@ -215,14 +59,9 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  dropColumn(event: CdkDragDrop<string[]>) {
-    console.log(event.container.data);
-    console.log('here');
-    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
-  }  
-
-  openDialog(buttonRef: HTMLElement): void {
+  openDialog(buttonRef: HTMLElement, columnIndex: number): void {
     const rect = buttonRef.getBoundingClientRect();
+    this.selectedColumnIndex = columnIndex;
 
     const dialogPosition = {
       top: `${rect.bottom}px`,
@@ -238,7 +77,15 @@ export class HomeComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('Dialog closed with result:', result);
+      if (result === 'add-card') {
+        this.openAddCard(columnIndex);
+      }
     });
+  }
+
+  openAddCard(columnIndex: number): void {
+    this.columns[columnIndex].adding = true;
+    this.dialog.closeAll;
   }
 
   addNewItem(columnIndex: number) {
@@ -247,6 +94,8 @@ export class HomeComponent implements OnInit {
       this.columns[columnIndex].items.push({ value: newItemValue, editing: false });
       this.saveChangesToLocalStorage();
       this.newItemValues[this.columns[columnIndex].name] = '';
+
+      this.columns[columnIndex].adding = false;
     }
   }
 
@@ -264,11 +113,18 @@ export class HomeComponent implements OnInit {
   }
 
   closeNewItemDiv(columnType: string) {
+    console.log('Closing new item div for column:', columnType);
+
     const columnIndex = this.columns.findIndex(column => column.name === columnType);
     if (columnIndex !== -1) {
         this.columns[columnIndex].adding = false;
         this.newItemValues[columnType] = '';
     }
+  }
+
+  closeColumnInput() {
+    this.addNewColumn = false;
+    this.newColumnTitle = '';
   }
 
   getColumnIndex(columnType: string): number {
@@ -285,6 +141,7 @@ export class HomeComponent implements OnInit {
       this.newColumnTitle = '';
       this.saveChangesToLocalStorage();
     }
+    this.addNewColumn = false;
   }
 
   openColumnInput() {
@@ -308,22 +165,14 @@ export class HomeComponent implements OnInit {
     const columnOrder = localStorage.getItem('columnOrder');
 
     if (columnsData) {
-      this.columns = JSON.parse(columnsData);
+        this.columns = JSON.parse(columnsData);
     }
 
     if (columnOrder) {
-      const orderedColumnNames: string[] = JSON.parse(columnOrder);
-      const orderedColumns = this.columns;
-    
-      orderedColumnNames.forEach(columnName => {
-        const foundColumn = this.columns.find(column => column.name === columnName);
-        if (foundColumn) {
-          orderedColumns.push(foundColumn);
-        }
-      });
-    
-      this.columns = orderedColumns;
+        const orderedColumnNames: string[] = JSON.parse(columnOrder);
+        this.columns = orderedColumnNames.map(columnName =>
+            this.columns.find(column => column.name === columnName)
+        ).filter(column => column) as { name: string, items: { value: string, editing: boolean }[], adding: boolean }[];
     }
-    
   }
 }
--- src/app/item-dialog/item-dialog.component.ts
+++ src/app/item-dialog/item-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-item-dialog',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-dialog.component.less']
 })
 export class ItemDialogComponent {
-  constructor() {}
+  constructor(public dialogRef: MatDialogRef<ItemDialogComponent>) {}
+
+  addCard() {
+    this.dialogRef.close('add-card');
+  }
 }
